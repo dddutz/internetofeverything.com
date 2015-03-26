69f9bd6922c253ee25009c1afacffec926fcb0d9
Derin Dutz <dddutz@gmail.com>
2015-03-25 17:13:17 -0700

    fixed database issue

--- routes/index.js
+++ routes/index.js
@@ -15,14 +15,17 @@ router.get('/hackathon', function(req, res) {
 
 /* GET database page. */
 router.get('/database', function(req, res) {
+	console.log("--- DATABASE URL ---");
+	console.log(process.env.DATABASE_URL);
+	console.log("--- END ---");
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
 	    client.query('SELECT * FROM test_table', function(err, result) {
 	    	done();
 	      	if (err) { 
 	      		console.error(err); 
-	      		response.send("Error " + err); 
+	      		res.send("Error " + err); 
 	      	} else { 
-	      		response.send(result.rows); 
+	      		res.send(result.rows); 
 	      	}
 	    });
 	});
